a8e92b5921e033e2fedc57bba8312b98add27195
Diego <wille.diegoricardo@gmail.com>
2022-06-16 13:53:49 -0300

    testes concluidos

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import logo from './logo.svg';
 import { add } from './features/markdown-preview/previewSlice';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { marked } from 'marked';
 
-function App() {
+function App() {  
   const text_preview = useSelector(state => state.preview.text)
   const dispatch = useDispatch()
   function handleEditor(event) {
@@ -12,23 +13,22 @@ function App() {
     let text = event.target.value;
     dispatch(add(text))
   }
-
+  function markdownConverter() {
+    let mkdown = marked.parse(text_preview)
+    return {__html: mkdown}
+  }  
   return (
     <div className="App container">
       <div className="row">
         <div className="col-1"></div>
         <div className="col-10">
-          <textarea name="markdown_editor" id="editor" onChange={handleEditor}></textarea>
+          <textarea name="markdown_editor" id="editor" onChange={handleEditor} value={text_preview}></textarea>          
         </div>
         <div className="col-1"></div>
-        <div className="col-1"></div>  
-        <div id="preview" className='col-10'>
-          <p>This is the preview</p>
-          <p>{text_preview}</p>
-          <p></p>
-        </div>
+        <div className="col-1"></div>          
+        <div id="preview" dangerouslySetInnerHTML={markdownConverter()} className='col-10'></div>
         <div className="col-1"></div>
-      </div>            
+      </div>      
     </div>
   );
 }
--- src/features/markdown-preview/previewSlice.js
+++ src/features/markdown-preview/previewSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const previewSlice = createSlice({
     name: 'markdown',
     initialState: {
-        text: ''
+        text: '# H1 \n ## H2 \n [google](https://google.com.br) \n\n`<div></div>` \n\n    console.log("hello") \n\n1. item1\n1. item1 \n\n > this is a blockquote. ![Tux](https://mdg.imgix.net/assets/images/tux.png?auto=format&fit=clip&q=40&w=100) **bold**'
     }, 
     reducers: {
         add: (state, text) => {
